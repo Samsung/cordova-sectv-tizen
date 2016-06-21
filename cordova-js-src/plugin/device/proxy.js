73e99a2f3fa45f2a3d44a8b023ec4a5fad503f43
Dayoung Kim <da_y.kim@samsung.com>
2016-06-21 19:18:15 +0900

    Add exception handling

--- cordova-js-src/plugin/device/proxy.js
+++ cordova-js-src/plugin/device/proxy.js
@@ -18,16 +18,23 @@ var tizen = require('cordova/platform');
 
 module.exports = {
     getDeviceInfo: function(success, error) {
-        setTimeout(function() {
-            success({
-                cordova: tizen.cordovaVersion,
-                platform: 'sectv-tizen',
-                model: webapis.productinfo.getModelCode(), // '15_HAWKP_UHD'
-                version: webapis.productinfo.getFirmware(), // 'T-HKPAKUC-0017.10'
-                uuid: webapis.productinfo.getDuid(), // 'U7CJYBPYKOKD6'
-                manufacturer: 'Samsung Tizen TV'
-            });
-        }, 0);
+        try {
+            setTimeout(function() {
+                success({
+                    cordova: tizen.cordovaVersion,
+                    platform: 'sectv-tizen',
+                    model: webapis.productinfo.getModelCode(), // '15_HAWKP_UHD'
+                    version: webapis.productinfo.getFirmware(), // 'T-HKPAKUC-0017.10'
+                    uuid: webapis.productinfo.getDuid(), // 'U7CJYBPYKOKD6'
+                    manufacturer: 'Samsung Tizen TV'
+                });
+            }, 0);
+        }
+        catch (e) {
+            setTimeout(function() {
+                error(e);
+            }, 0);
+        }
     }
 };
 
